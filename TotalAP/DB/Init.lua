49fe188c5d2f695101935a9077778d2232acba49
Sacred Duckwhale <duckwhale@mail.ru>
2017-04-13 16:22:24 +0200

    Deleted obsolete prezip folder

--- TotalAP/DB/Init.lua
+++
@@ -1,7 +0,0 @@
--- [[ Initialise TotalArtifactPowerDB ]] --
-
-if not TotalArtifactPowerDB then
-	TotalArtifactPowerDB = {};
-end
-
--- Might add more stuff here later. For the time being, the individual DB files will add their data to populate the DB, which can then be used by the addon itself
