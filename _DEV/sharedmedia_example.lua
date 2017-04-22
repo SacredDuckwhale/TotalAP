7c31a52d19415472d9ea6835f9faf487f8b7c422
Sacred Duckwhale <duckwhale@mail.ru>
2017-04-22 18:01:17 +0200

    Removing _DEV

--- _DEV/sharedmedia_example.lua
+++
@@ -1,6 +0,0 @@
--- LIBRARIES
-local smed = LibStub("LibSharedMedia-3.0")
-
-	local texture = smed:Fetch("statusbar", db.texture)
-	
-	locText:SetFont(smed:Fetch("font", db.font), db.fontsize, db.outline and "OUTLINE" or nil)
