26a8f5c70aca0cce71339bd273532edcc62b8a95
Sacred Duckwhale <duckwhale@mail.ru>
2017-06-03 01:19:53 +0200

    Added hopefully last locales fix
    Somehow I must've forgotten to add this back in after getting lost in
    the depths of Curse's packaging madness... but it ought to be there

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,3 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TotalAP", "deDE")
+L = L or {}
 
---@localization(locale="deDE", format="lua_additive_table", namespace="", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="english")@
+--@localization(locale="deDE", format="lua_additive_table", namespace="", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="ignore")@
--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -1,3 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TotalAP", "zhTW")
+L = L or {}
 
 --@localization(locale="zhTW", format="lua_additive_table", namespace="", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="ignore")@
