4e9ab4ad20958fefa745d5586cae4114e72b700a
Sacred Duckwhale <duckwhale@mail.ru>
2017-06-02 20:31:28 +0200

    Attempted another CF packager fix

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,4 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TotalAP", "deDE")
 
 
---@localization(locale="deDE", format="lua_additive_table", namespace="Base Namespace", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="english")@
+--@localization(locale="deDE", format="lua_additive_table", namespace="", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="english")@
--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -1,3 +1,3 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TotalAP", "zhTW")
 
---@localization(locale="zhTW", format="lua_additive_table", namespace="Base Namespace", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="zhTW", format="lua_additive_table", namespace="", same-key-is-true="true", handle-subnamespaces="none", handle-unlocalized="ignore")@
