5d8cb68a512d7aa5760208f835c836c1fc915b44
Sacred Duckwhale <duckwhale@mail.ru>
2017-07-01 19:14:20 +0200

    Added more incomplete tests for FormatShort

--- Tests/test.lua
+++ Tests/test.lua
@@ -1,10 +1,12 @@
 -- Experimental test
+-- TODO: Move global environment to setup / different files based on which parts are being mocked (so they can be called by individual test's SetUp routines)
 
 -- Required Lua modules
 local luaunit = require("luaunit")
 
 -- Settings
 local addonName = "TotalAP"
+local locale = "enUS"
 
 local root = "..\\" 
 local toc = addonName .. ".toc"
@@ -26,6 +28,11 @@ GetAddOnMetadata = function(addon, value)
 	if addon == addonName then
 		return "1.3.1 (r-23)"
 	end
+	
+end
+GetLocale = function()
+
+	return locale or "enUS"
 	
 end
 
@@ -101,7 +108,7 @@ T = {}
 
 
 -- Read TOC file and load all addon-specific lua and xml files (will extract embeds, but not parse actual XML)
-function readTOC(filePath)
+function readTOC(filePath) -- TODO: Split this up into TOC Parser and Lua Loader
 
 	-- Establish load order and assemble list of all addon files (by reading the TOC file)
 	local addonFiles = {}
@@ -155,5 +162,70 @@ end
 
 -- Read TOC
 readTOC(root .. toc)
+
+-- TODO: Move individual tests to other files (to turn them into a test suite that can be run individually)
+
+-- Add tests for individual modules
+TestCore = {}
+TestGUI = {} -- TODO: This might be impossible
+TestControllers = {}
+TestUtils = {}
+
+-- Core\Utils\Colours.lua
+
+-- Tests for Core\Utils\Format.lua
+TestFormat = {}
+
+-- Since value is nil, it should not format anything (and return nothing, which implicitly means "returns nil")
+function TestFormat:testNoParameters() 
+	luaunit.assertEquals(T.Utils.FormatShort(), nil)
+end
+
+-- If given a value that isn't a number, it should also implicitly "return nil"
+function TestFormat:testOneInvalidParameter()
+	luaunit.assertEquals(T.Utils.FormatShort("Long time no siege!"), nil)
+end
+
+-- Given a single numeric value, it should return the formatString and ready-to-insert value (this is identical to format = false and locale = legacy/default)
+function TestFormat:testOneValidParameter()
+	
+	local actualResultsTable, str, num = {}, "", 0
+	
+	-- Multiple return values -> need to check both for each test (use this function as a shortcut)
+	local function compareResults(value, expectedResultsTable)
+		
+		str, num = T.Utils.FormatShort(value)
+		actualResultsTable = { str, num }
+		luaunit.assertEquals(actualResultsTable,  expectedResultsTable )
+		
+	end
+	
+	compareResults( 0.5, { "%d", 1 })
+	compareResults(5.5,  { "%d", 6 })
+	compareResults(55,  { "%d", 55 })
+	compareResults(550,  { "%d", 550 })
+	compareResults(5500,  { "%d", 5500 })
+	compareResults(55000,  { "%.1fk", 55 })
+	compareResults(550000,  { "%.0fk", 550 })
+	compareResults(5500000,  { "%.2fm", 5.5 })
+	compareResults(55000000,  { "%.1fm", 55 })
+	compareResults(550000000,  { "%.1fm", 550 })
+	compareResults(5500000000,  { "%.1fb", 5.5 })
+	
+end
+
+-- Given two valid parameters, the formatting should be applied directly
+function TestFormat:testTwoParameters()
+
+	
+
+end
+
+-- Given three parameters, the given locale should be used to determine the number format that will then be applied
+function TestFormat:testThreeParameters()
+
+	
+
+end
 
 os.exit( luaunit.LuaUnit.run() )
