a15148826ac8faab86c24333ed2ed86a22c786d1
Sacred Duckwhale <duckwhale@mail.ru>
2017-07-16 19:02:28 +0200

    Re-added DefaultView stub

---
+++ Core/GUI/Views/DefaultView.lua
@@ -0,0 +1,44 @@
+  ----------------------------------------------------------------------------------------------------------------------
+    -- This program is free software: you can redistribute it and/or modify
+    -- it under the terms of the GNU General Public License as published by
+    -- the Free Software Foundation, either version 3 of the License, or
+    -- (at your option) any later version.
+	
+    -- This program is distributed in the hope that it will be useful,
+    -- but WITHOUT ANY WARRANTY; without even the implied warranty of
+    -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    -- GNU General Public License for more details.
+
+    -- You should have received a copy of the GNU General Public License
+    -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+----------------------------------------------------------------------------------------------------------------------
+
+--- 
+-- @module GUI
+
+--- DefaultView.lua.
+-- The classic TotalAP GUI as it was used in earlier versions
+-- @section GUI
+
+
+local addonName, TotalAP = ...
+if not TotalAP then return end
+
+DefaultView = {}
+
+local function CreateNew(self)
+	
+	local ViewObject = {}
+
+	setmetatable(ViewObject, self) -- The new object inherits from this class
+	self.__index = TotalAP.GUI.View -- ... and this class inherits from the generic View template
+	
+	-- TODO...
+	
+	return ViewObject
+	
+end
+
+TotalAP.Views.DefaultView = DefaultView
+
+return DefaultView
