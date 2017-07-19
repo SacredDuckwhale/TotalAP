399bc1bf16478a8adb71bebe823ae64c56b6ebfc
Sacred Duckwhale <duckwhale@mail.ru>
2017-07-19 12:35:34 +0200

    Removed unnecessary bloat in existing code

--- Core/Cache.lua
+++ Core/Cache.lua
@@ -290,32 +290,19 @@ local function GetNumIgnoredSpecs(fqcn)
 	
 end
 
---- Removes all specs from the ignored specs list for a given character (defaults to currently used character if none is given)
--- @param[opt] fqcn  Fully-qualified character name that will have their specs "IsIgnored" setting reset
--- TODO: This doesn't belong here
+	
+	
+	
+	
+--- Removes all specs from the ignored specs list for a given character
+-- @param[opt] fqcn Fully-qualified character name (defaults to currently used character if none is given)
 local function UnignoreAllSpecs(fqcn)
 	
-	if not TotalArtifactPowerCache then return end -- Skip unignore if cache isn't initialised or this is called before the addon loads
-	
-	-- TODO: DRY
-	local characterName, realm
-	
-	if fqcn then 
-		characterName, realm = fqcn:match("(%.+)%s-%s(%.)+")
-	end
-	
-	if not characterName or not realm then -- Use currently active character
-		
-		characterName = UnitName("player")
-		realm = GetRealmName()
-		
-	end
-		 
-	local key = format("%s - %s", characterName, realm)	 
+	fqcn = fqcn or TotalAP.Utils.GetFQCN() 
 	
 	for i = 1, GetNumSpecializations() do -- Remove spec from "ignore list" (more precisely, remove "marked as ignored" flag for all cached specs of the active character)
 	
-		if TotalArtifactPowerCache[key] and TotalArtifactPowerCache[key][i] then TotalArtifactPowerCache[key][i]["isIgnored"] = false end
+		SetValue(fqcn, i, "isIgnored", false)
 	
 	end
 	
