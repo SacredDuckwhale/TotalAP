e23485db0ae40402a98392f003e8fe6eece15d9c
Sacred Duckwhale <duckwhale@mail.ru>
2017-08-10 19:14:57 +0200

    Added UnitX mockup API (used by some libraries)

--- Tests/mock_wowapi.lua
+++ Tests/mock_wowapi.lua
@@ -29,6 +29,51 @@ UnitName = function(unit)
 
 end
 
+UnitClass = function(unit)
+
+	if unit == "player" then
+		
+		local classDisplayName, class, classID = "Rogue", "ROGUE", 4
+		return classDisplayName, class, classID
+		
+	end	
+
+end
+
+UnitRace = function(unit)
+
+	if unit == "player" then
+		
+		local raceName, raceId = "Human", "Human"
+		return raceName, raceId 
+		
+	end	
+
+end
+
+UnitFactionGroup = function(unit)
+
+	if unit == "player" then
+	
+		englishFaction, localizedFaction = "Alliance", "Alliance"
+		return englishFaction, localizedFaction
+		
+	end	
+
+end
+
+GetNumSpecializations = function()
+
+	return 3
+	
+end
+
+GetSpecialization = function()
+
+	return 1
+	
+end
+
 -- WOW API objects
 GameTooltip = {}
 function GameTooltip:HookScript(triggerEvent, scriptFunction)
