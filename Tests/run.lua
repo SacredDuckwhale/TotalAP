545f9d381f2e927dac5303a4f9ed9241992bce55
Sacred Duckwhale <duckwhale@mail.ru>
2017-09-06 19:53:02 +0200

    Remved test suite overhead code
    Handled by WBT

--- Tests/run.lua
+++ Tests/run.lua
@@ -3,19 +3,7 @@
 -- TODO: Tests for locales other than enUS, deDE, ruRU, frFR, zhTW (those that use English by default, until users have complained and provided a better format)
 
 
--- Required Lua modules
-luaunit = require("luaunit") -- stored in global so that testing suites can access it directly
 
--- Required WBT modules
-local TOC = require("Utils/TOC")
-
--- TODO: Move elsewhere?
-local inspect = require('inspect')
-function dump(value)
-
-	print(inspect(value))
-
-end
 
 
 -- Testing suites
@@ -26,19 +14,11 @@ require("Core\\Utils\\test_fqcn")
 
 
 -- Settings
-addonName = "TotalAP"
 locale = "enUS"
 region = "EU"
 realm = "Outland"
 
-local args = { ... } -- Pass project path from WBT
-local path = args[1]
 
-local root = "..\\"  -- path from tests subfolder to addon root dir
-local toc = addonName .. ".toc"
-
--- Read TOC file and 
-TOC:Read((path and path .. "/") or root, toc)
 
 function TotalAP.Debug(msg)
 	print("DEBUG: " .. msg)
