f9ee54b5c47df73b48e910288436bcc464c87adf
Sacred Duckwhale <duckwhale@mail.ru>
2017-09-06 19:56:26 +0200

    Cleanup

--- Tests/run.lua
+++ Tests/run.lua
@@ -1,30 +1,21 @@
--- Full testing suite (work in progress) -> Mainly focuses on non-GUI unit tests
--- TODO: Move global environment to setup / different files based on which parts are being mocked (so they can be called by individual test's SetUp routines)
--- TODO: Tests for locales other than enUS, deDE, ruRU, frFR, zhTW (those that use English by default, until users have complained and provided a better format)
-
-
-
-
-
--- Testing suites
+-- Add test suites to luaunit queue
 require("Core\\test_cache")
 require("Core\\Utils\\test_colours")
 require("Core\\Utils\\test_format")
 require("Core\\Utils\\test_fqcn")
 
 
--- Settings
+-- Settings (TODO: Move to WBT/mock environment setup)
 locale = "enUS"
 region = "EU"
 realm = "Outland"
 
 
-
+-- Overwrite addon functions (for testing purposes only)
 function TotalAP.Debug(msg)
 	print("DEBUG: " .. msg)
 end
 
 -- Run all tests that have been queued
 local exitCode = luaunit.LuaUnit.run("--output", "TAP")
-
 return function() return exitCode end
